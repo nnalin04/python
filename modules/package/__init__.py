dafdb0f8ab1e22c891fe406cd7bac01abef45712
nishitnalinsrivastava <nnalin04@gmail.com>
2025-08-23 13:13:14 +0530

    Initial Commit

---
+++ modules/package/__init__.py
@@ -0,0 +1,4 @@
+'''
+__init__.py
+This is a special file used in Python to define packages and initialize the namespace.
+'''
---
+++ modules/test.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackages.mult import *
+
+print(addition(2, 3))
+print(substraction(2, 3))
+print(multiply(2, 3))
+print(devide(6,3))
